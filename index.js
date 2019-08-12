1b429b13c1c3028ea13e38199a9409dad5051aaa
Jeffrey Musselman <jeffmussels@gmail.com>
2019-08-12 01:39:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('.ranked-list')
+  for (var i = 0; i < lis.length; i+=1) {
+    lis[i].innerHTML = (parseInt(lis[i]) + n).toString
+  }
+}
+
+function deepestChild() {
+  var nodes = document.querySelector('div#grand-node')
+  var kids = nodes.children[0]
+  
+  while (kids) {
+    nodes = kids
+    kids = nodes.children[0]
+  }
+  return (nodes)
+}
+    
+  
+ 
